d1603fadcc1e4b75379fdf28c1023c96dad1da27
Welvin Medina <welvs.medina@gmail.com>
2016-07-03 18:39:19 +0800

    Initialize

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,8 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/semantic/dist/semantic.css'
       ],
       js: [
         'public/lib/angular/angular.js',
